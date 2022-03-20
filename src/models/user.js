06bb54be504d6d0b9afcf05240bd40283cd3d3d7
Prithu Singh <prithusingh0327@gmail.com>
2022-03-20 20:23:01 +0530

    Added password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("user", {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -39,5 +40,17 @@ const User = mongoose.model("user", {
         },
     },
 });
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
