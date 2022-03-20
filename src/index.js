8eaa3fce79a2f207a2daba902bbf0e4111a83a42
Prithu Singh <prithusingh0327@gmail.com>
2022-03-20 15:34:51 +0530

    Added get user and all users endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
+const { get } = require("express/lib/response");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,10 +15,36 @@ app.post("/users", (req, res) => {
 
     user.save()
         .then(() => {
-            res.send(user);
+            res.status(201).send(user);
         })
         .catch((error) => {
             res.status(400).send(error);
+        });
+});
+
+app.get("/users", (req, res) => {
+    User.find({})
+        .then((users) => {
+            res.send(users);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+});
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send();
+            }
+
+            res.send(user);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
         });
 });
 
@@ -26,7 +53,7 @@ app.post("/tasks", (req, res) => {
 
     task.save()
         .then(() => {
-            res.send(task);
+            res.status(201).send(task);
         })
         .catch((error) => {
             res.status(400).send(error);
