736263f0922b1c55a0f012261cf4e0bbeffec70b
Prithu Singh <prithusingh0327@gmail.com>
2022-03-24 00:13:49 +0530

    Added avatar delete route

--- src/routers/user.js
+++ src/routers/user.js
@@ -89,7 +89,6 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-    dest: "avatars",
     limits: {
         fileSize: 1000000,
     },
@@ -103,8 +102,11 @@ const upload = multer({
 
 router.post(
     "/users/me/avatar",
+    auth,
     upload.single("avatar"),
-    (req, res) => {
+    async (req, res) => {
+        req.user.avatar = req.file.buffer;
+        await req.user.save();
         res.send();
     },
     (error, req, res, next) => {
@@ -113,5 +115,11 @@ router.post(
         });
     }
 );
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+});
 
 module.exports = router;
